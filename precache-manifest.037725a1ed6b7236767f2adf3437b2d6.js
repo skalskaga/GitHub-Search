baf7125d9c3ab13611c01b45ce4a2e4e9bbe1e93
skalskaga <agnieszkaskalska93@gmail.com>
2019-11-13 05:43:13 +0100

    Updates

---
+++ precache-manifest.037725a1ed6b7236767f2adf3437b2d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b801c06e1ed3d7e7fd59399a713c98",
+    "url": "/GitHub-Search/index.html"
+  },
+  {
+    "revision": "2795ce5c2e7b4b927c85",
+    "url": "/GitHub-Search/static/css/main.15c9a6aa.chunk.css"
+  },
+  {
+    "revision": "b69a6b5872ede0cae4fd",
+    "url": "/GitHub-Search/static/js/2.6f48c54c.chunk.js"
+  },
+  {
+    "revision": "2795ce5c2e7b4b927c85",
+    "url": "/GitHub-Search/static/js/main.df4e9ee2.chunk.js"
+  },
+  {
+    "revision": "8832fd27ab5d1cfd465d",
+    "url": "/GitHub-Search/static/js/runtime-main.0f76cb45.js"
+  }
+]);
